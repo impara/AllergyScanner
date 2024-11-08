e44cea1008e88533c723ef776a834c8d2b9b41c0
Amer <impara1@gmail.com>
2024-11-08 16:48:39 +0100

    configuration error

--- app.config.js
+++ app.config.js
@@ -29,6 +29,11 @@ module.exports = ({ config }) => {
             resizeMode: "contain",
             backgroundColor: "#ffffff"
         },
+        updates: {
+            url: "https://u.expo.dev/c2ceb6a3-210e-4d75-b3cf-38878dd25b98",
+            enabled: true,
+            checkAutomatically: "ON_LOAD"
+        },
         android: {
             package: "com.pureplate",
             versionCode: 1,
@@ -107,25 +112,26 @@ module.exports = ({ config }) => {
                 }
             }]
         ],
-        updates: {
-            url: "https://u.expo.dev/c2ceb6a3-210e-4d75-b3cf-38878dd25b98",
-            enabled: true,
-            checkAutomatically: "ON_LOAD"
-        },
-        expo: {
-            ...config.expo,
-            extra: {
-                eas: {
-                    projectId: 'c2ceb6a3-210e-4d75-b3cf-38878dd25b98'
-                }
+        extra: {
+            eas: {
+                projectId: 'c2ceb6a3-210e-4d75-b3cf-38878dd25b98'
             },
-            android: {
-                config: {
-                    googleMobileAds: {
-                        delayAppMeasurementInit: true
-                    }
-                }
-            }
+            GOOGLE_IOS_CLIENT_ID: process.env.GOOGLE_IOS_CLIENT_ID,
+            GOOGLE_ANDROID_CLIENT_ID: process.env.GOOGLE_ANDROID_CLIENT_ID,
+            GOOGLE_EXPO_CLIENT_ID: process.env.GOOGLE_EXPO_CLIENT_ID,
+            FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || null,
+            FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || null,
+            FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || null,
+            FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET || null,
+            FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID || null,
+            FIREBASE_APP_ID: process.env.FIREBASE_APP_ID || null,
+            FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID || null,
+            FOOD_REPO_API_KEY: process.env.FOOD_REPO_API_KEY || null,
+            ADMOB_ANDROID_APP_ID: process.env.ADMOB_ANDROID_APP_ID || null,
+            ADMOB_IOS_APP_ID: process.env.ADMOB_IOS_APP_ID || null,
+            ADMOB_ANDROID_REWARDED_AD_UNIT_ID: process.env.ADMOB_ANDROID_REWARDED_AD_UNIT_ID || null,
+            ADMOB_IOS_REWARDED_AD_UNIT_ID: process.env.ADMOB_IOS_REWARDED_AD_UNIT_ID || null,
+            FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL || null,
         }
     };
 };
